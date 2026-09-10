83a01765a1387508fb414ff0eef0e8d3e1f524af
sindhuja-isgntech <sindhuja.konduru@isigntech.com>
2026-09-10 17:04:04 +0530

    Initialize employee HR management project with Maven setup and basic application structure

---
+++ src/main/java/com/hrms/employee/EmployeeHrManagementApplication.java
@@ -0,0 +1,13 @@
+package com.hrms.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeHrManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeHrManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hrms/employee/EmployeeHrManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrms.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeHrManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
